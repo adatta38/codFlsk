453e4da8802748c4bad3e45ebef2054808bac3e8
Abhishek Datta <adatta@expedia.com>
2018-07-17 15:15:32 +0530

    Testing Resources

--- hello_world_flask.py
+++ hello_world_flask.py
@@ -1,5 +1,31 @@
 from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
+
+
 app = Flask(__name__)
+
+api = Api(hello_world_flask)
+
+
+class Add(Resource):
+    def post(self):
+        postedData = request.get_json()
+        x = postedData["x"]
+        y = postedData["y"]
+        x = int(x)
+        y = int(y)
+        ret = x + y
+        retMap = {
+        'Message':ret,
+        'Status Code':200
+        }
+        return jsonify(retMap)
+
+api = add_resource(Add, "/add")
+
+
+
+
 
 @app.route('/')
 
